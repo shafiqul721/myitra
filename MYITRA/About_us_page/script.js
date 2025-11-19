c95e111d5413a2120131f3f38bab3acc5fa7230c
shafiqul721 <mohammadshuvo721@gmail.com>
2025-11-20 07:58:56 +0800

    Enhance navigation link interactivity
    Added cursor style to clickable elements for better UX.

--- MYITRA/About_us_page/script.js
+++ MYITRA/About_us_page/script.js
@@ -1,18 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
+
+  // Navigation link mapping
   const links = {
     "home-link": "../Home page/index.html",
     "aboutus-link": "../About us page/index.html",
     "whychooseus-link": "../Why choose us page/index.html",
     "footer-logo": "../Home page/index.html",
-    "fhome-link": "../Home page/index.html",
+    "fhome-link": "../Home page/index.html"
   };
 
+  // Attach click events to all defined links
   Object.entries(links).forEach(([id, page]) => {
     const element = document.getElementById(id);
     if (element) {
+      element.style.cursor = "pointer"; // make it clickable
       element.addEventListener("click", () => {
         window.location.href = page;
       });
     }
   });
+
 });
